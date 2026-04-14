8c5a41ce5e32c6e7e256b829c060cdde379887fe
Bernd Donner <bernd.donner@sabel.com>
2026-04-14 11:22:59 +0200

    7. Fix-Versuch für den Beenden-Bug

--- src/row_taker/server/network_server.py
+++ src/row_taker/server/network_server.py
@@ -193,7 +193,6 @@ def run_network_server(
             if network_server.should_shutdown():
                 logger.info("session ended and no participants connected anymore; server shutting down")
                 break
-        logger.info("network server main loop finished")
             try:
                 conn, addr = listener.accept()
             except TimeoutError:
@@ -205,4 +204,4 @@ def run_network_server(
             )
             thread.start()
 
-
+        logger.info("network server main loop finished")
